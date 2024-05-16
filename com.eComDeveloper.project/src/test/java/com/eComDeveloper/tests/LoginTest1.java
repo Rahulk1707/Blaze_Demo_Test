86101cc26a7f3fe07e253937672f419b24a69fd6
Rahulk1707 <146314120+Rahulk1707@users.noreply.github.com>
2024-05-17 00:28:37 +0530

    Update LoginTest1.java
    Some minor changes.

--- com.eComDeveloper.project/src/test/java/com/eComDeveloper/tests/LoginTest1.java
+++ com.eComDeveloper.project/src/test/java/com/eComDeveloper/tests/LoginTest1.java
@@ -7,26 +7,27 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.eComDeveloper.pages.LoginPage1;
-
 public class LoginTest1 {
 	
 	private WebDriver driver;
 	private String baseUrl = "https://www.facebook.com";
 	
 	@BeforeMethod
-	public void setUp() {
+	public void setUp() throws InterruptedException {
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get(baseUrl);
+		Thread.sleep(2000);
 	}
 	
 	@Test
-	public void loginTest() {
-		LoginPage1 lp1 = new LoginPage1(driver);
-		lp1.enterUsername("rahulk@gmail.com");
-		lp1.enterPassword("1234567");
-		lp1.clickLogin();
+	public void loginTest() throws InterruptedException {
+		driver.findElement(By.id("email")).sendKeys("rahulk@gmail.com");
+		Thread.sleep(2000);
+		driver.findElement(By.id("pass")).sendKeys("rahulk123");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[contains(@id,'u_0_5')]")).click();
+		Thread.sleep(5000);
 	}
 	
 	@AfterMethod
